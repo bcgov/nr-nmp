f1428c27be1528d3875fbd31c9200be8ea6100a0
Dallas Richmond <dallas.richmond@gov.bc.ca>
2024-11-19 14:35:04 -0800

    Header renders on page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,15 @@
 import StyledApp from './App.styles';
+import { Header } from './components/common';
 
 /**
  * @summary The root component of the application.
  */
-const App = () => (
-  <StyledApp>
-    <div />
-    <h1>Vite + React</h1>
-    <div className="card">
-      <p>
-        Edit <code>src/App.tsx</code> and save to test HMR
-      </p>
-    </div>
-    <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-  </StyledApp>
-);
+function App() {
+  return (
+    <StyledApp>
+      <Header />
+    </StyledApp>
+  );
+}
 
 export default App;
---
+++ frontend/src/components/common/index.ts
@@ -0,0 +1,4 @@
+import Header from './Header/Header';
+
+// eslint-disable-next-line import/prefer-default-export
+export { Header };
