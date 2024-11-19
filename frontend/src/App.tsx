38d25a4177f40ccd4c2bcfaf2d973fac4593cda1
Dallas Richmond <dallas.richmond@gov.bc.ca>
2024-11-19 14:03:36 -0800

    Added typography

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,11 @@
-import './App.css';
+import StyledApp from './App.styles';
+import '@bcgov/bc-sans/css/BC_Sans.css';
 
+/**
+ * @summary The root component of the application.
+ */
 const App = () => (
-  <>
+  <StyledApp>
     <div />
     <h1>Vite + React</h1>
     <div className="card">
@@ -10,7 +14,7 @@ const App = () => (
       </p>
     </div>
     <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-  </>
+  </StyledApp>
 );
 
 export default App;
---
+++ frontend/src/constants/screensizes.tsx
@@ -0,0 +1,7 @@
+const screensizes = {
+  mobile: '480px',
+  tablet: '768px',
+  desktop: '1025px',
+};
+
+export default screensizes;
