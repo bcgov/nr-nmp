78b57141d897fe0db617ed58fc95f9e7fbce5c2f
raarielgrace <raarielgrace@gmail.com>
2024-12-09 16:13:07 -0800

    feat: [NMP-83] Add basic keycloak (#85)
    Co-authored-by: Dallas Richmond <dallas.richmond@gov.bc.ca>
    Co-authored-by: dallascrichmond <113049138+dallascrichmond@users.noreply.github.com>

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,19 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { SSOProvider } from '@bcgov/citz-imb-sso-react';
 import App from './App.tsx';
+import { env } from '@/env';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <SSOProvider
+      backendURL={env.VITE_BACKEND_URL}
+      idpHint="idir"
+    >
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </SSOProvider>
   </StrictMode>,
 );
