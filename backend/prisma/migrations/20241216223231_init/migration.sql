afd1782d30a3c6655ab98bea27b686d92cab4d75
Dallas Richmond <dallas.richmond@gov.bc.ca>
2024-12-16 14:35:02 -0800

    Updated volumes

---
+++ backend/prisma/migrations/20241216223231_init/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "Region" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Region_pkey" PRIMARY KEY ("id")
+);
---
+++ backend/start.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+pwd
+# Run migrations
+npx prisma db push --schema='prisma/schema.prisma' --skip-generate
+# Seed data
+# npx prisma db seed
