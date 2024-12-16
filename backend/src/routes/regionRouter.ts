cd344e9da33f38b4dcd92d708cf3944bc1e0f1ad
Dallas Richmond <dallas.richmond@gov.bc.ca>
2024-12-16 12:01:33 -0800

    Prisma initialized and test region model defined

--- backend/src/routes/regionRouter.ts
+++ backend/src/routes/regionRouter.ts
@@ -1,10 +1,10 @@
 // Root|/api endpoint router
 // Any routes in /api can be accessed through here
 import express from 'express';
-import { getRegions, CreateRegion } from '../controllers/regionsControler';
+import { getRegions, CreateRegion } from '../controllers/regionsController';
 
-const router = express.Router();
+const regionRouter = express.Router();
 
-router.route('/regions').get(getRegions).post(CreateRegion);
+regionRouter.route('/regions').get(getRegions).post(CreateRegion);
 
-export default router;
+export default regionRouter;
