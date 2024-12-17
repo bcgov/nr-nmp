9297fc9bac91aa497f4a01b9941e67b90ec1d2e5
Ariel Rotter-Aboyoun <raarielgrace@gmail.com>
2024-12-17 12:46:50 -0800

    Altered code to use forked repo

--- frontend/src/routes/ProtectedRoute.tsx
+++ frontend/src/routes/ProtectedRoute.tsx
@@ -1,5 +1,5 @@
-import { useSSO } from '@bcgov/citz-imb-sso-react';
-import { ReactNode } from 'react';
+import { AuthContext, useSSO } from '@bcgov/citz-imb-sso-react';
+import { ReactNode, useContext } from 'react';
 import { Navigate } from 'react-router-dom';
 
 type ProtectedRouteProps = {
@@ -8,7 +8,16 @@ type ProtectedRouteProps = {
 };
 
 export default function ProtectedRoute({ requiredRole, children }: ProtectedRouteProps) {
-  const { hasRoles } = useSSO();
+  const authContext = useContext(AuthContext);
+  const { hasRoles, user } = useSSO();
+
+  // Return early if the user data isn't available yet
+  if (
+    authContext.state.isStale ||
+    (authContext.state.userInfo !== undefined && user === undefined)
+  ) {
+    return null;
+  }
 
   if (!hasRoles([requiredRole])) {
     return (
