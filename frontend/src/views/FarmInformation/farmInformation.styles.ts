404f43e6ac037a24eb2f6b5acdfca539c6d2dfa2
Dallas Richmond <dallas.richmond@gov.bc.ca>
2025-01-10 13:25:26 -0800

    Updated FarmInformation styling

--- frontend/src/views/FarmInformation/farmInformation.styles.ts
+++ frontend/src/views/FarmInformation/farmInformation.styles.ts
@@ -4,23 +4,6 @@
 import styled from '@emotion/styled';
 import screenSizes from '../../constants/screenSizes';
 
-export const Card = styled.div`
-  background-color: rgba(255, 255, 255, 0.8);
-  height: 500px;
-  width: 600px;
-  padding-top: 0;
-  justify-content: flex-start;
-  align-items: center;
-  display: flex;
-  flex-direction: column;
-  object-fit: scale-down;
-  border-radius: 8px;
-  box-shadow: 0 8px 16px rgba(0, 0, 0, 0.1);
-  padding: 20px;
-  text-align: center;
-  position: relative;
-`;
-
 export const CardHeader = styled.div`
   background-color: rgba(200, 200, 200, 0.3);
   padding: 0;
@@ -87,7 +70,7 @@ export const ButtonWrapper = styled.div`
   bottom: 16px;
   right: 16px;
   button {
-    width: 120px;
-    height: 60px;
+    width: 80px;
+    height: 40px;
   }
 `;
