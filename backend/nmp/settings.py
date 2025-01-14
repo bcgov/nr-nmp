81dddccad343dc40d5d3d46085ca945b0290a31a
Ariel Rotter-Aboyoun <raarielgrace@gmail.com>
2025-01-14 14:04:21 -0800

    Another allowed hosts test

--- backend/nmp/settings.py
+++ backend/nmp/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-y*-zsvc=qasr^th3%4m8zmt!t-s_2&yuzlyk^h1*%ufic+l_k5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '0.0.0.0', '.devops.gov.bc.ca']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', '.apps.silver.devops.gov.bc.ca']
 
 
 # Application definition
