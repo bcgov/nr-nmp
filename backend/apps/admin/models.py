c43769b63325ef2b79ed94ddabc591906fdba4a7
Paul Garewal <paul.garewal@gov.bc.ca>
2025-01-20 11:35:45 -0800

    successful testing of crop migration

---
+++ backend/apps/admin/models.py
@@ -0,0 +1,3 @@
+from django.db import models
+
+# Models for the admin app will go here 
--- backend/apps/crops/models.py
+++ backend/apps/crops/models.py
@@ -1,12 +1,9 @@
 from django.db import models
 
 class CropTypes(models.Model):
-    # This model errors because it doesn't have a primary key
-    # Django tries to treat id as the primary key but it's non-unique
-    # Trying to add some fake primary key that doesn't exist in the table also errors
-    # This composite key is only supported in an alpha build of Django
-    # But even the alpha build doesn't support composite keys on the admin portal
-    # pk = models.CompositePrimaryKey('id', 'staticdataversionid')
+    # Using an auto-incrementing primary key
+    crop_id = models.AutoField(primary_key=True)
+    # Existing fields
     id = models.IntegerField()
     name = models.CharField(max_length=100)
     covercrop = models.BooleanField()
@@ -16,5 +13,9 @@ class CropTypes(models.Model):
     staticdataversionid = models.IntegerField()
 
     class Meta:
-        managed = False
+        managed = True  # Changed to True so Django manages this table
         db_table = 'crop_types'
+        unique_together = ['id', 'staticdataversionid']  # Maintaining the composite uniqueness
+
+    def __str__(self):
+        return self.name
