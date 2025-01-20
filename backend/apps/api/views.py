7747e5ab49b326df234198c4075edd769ebea7a8
raarielgrace <raarielgrace@gmail.com>
2025-01-20 13:57:07 -0800

    feat: [NMP-144] CropTypes endpoint (#142)
    Co-authored-by: Dallas Richmond <dallas.richmond@gov.bc.ca>

---
+++ backend/apps/api/views.py
@@ -0,0 +1,46 @@
+from rest_framework import status, viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from apps.crops.models import CropTypes
+from apps.crops.serializers import CropTypesSerializer
+
+class APIViewSet(viewsets.ViewSet):
+    @action(detail=True, methods=['get'])
+    def cropTypes(self, request, pk=None):
+        crop_types = CropTypes.objects.all()
+        serializer = CropTypesSerializer(crop_types, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+from rest_framework import status, viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from apps.crops.models import CropTypes
+from apps.crops.models import Crops
+from apps.crops.serializers import CropTypesSerializer
+from apps.crops.serializers import CropsSerializer
+from apps.animals.models import *
+from apps.animals.serializers import *
+from apps.fertilizers.models import Fertilizers
+from apps.fertilizers.serializers import FertilizersSerializer
+
+class APIViewSet(viewsets.ViewSet):
+    @action(detail=True, methods=['get'])
+    def cropTypes(self, request, pk=None):
+        crop_types = CropTypes.objects.all()
+        serializer = CropTypesSerializer(crop_types, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    @action(detail=True, methods=['get'])
+    def crops(self, request, pk=None):
+        crops = Crops.objects.all()
+        serializer = CropsSerializer(crops, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    @action(detail=True, methods=['get'])
+    def animals(self, request, pk=None):
+        animals = Animals.objects.all()
+        serializer = AnimalsSerializer(animals, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    @action(detail=True, methods=['get'])
+    def fertilizers(self, request, pk=None):
+        fertilizers = Fertilizers.objects.all()
+        serializer = FertilizersSerializer(fertilizers, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
