c100315d76805120d0977a1afcc8a97bfc1546e0
Dallas Richmond <dallas.richmond@gov.bc.ca>
2025-01-20 13:11:59 -0800

    TEST cropstype endpoint

--- backend/apps/api/views.py
+++ backend/apps/api/views.py
@@ -1,8 +1,12 @@
 from rest_framework import status, viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from backend.apps.crops.models import CropTypes
+from backend.apps.crops.serializers import CropTypesSerializer
 
 class APIViewSet(viewsets.ViewSet):
     @action(detail=True, methods=['get'])
     def dummy(self, request, pk=None):
-        return Response('TEST', status=status.HTTP_200_OK)
+        crop_types = CropTypes.objects.all()
+        serializer = CropTypesSerializer(crop_types, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
