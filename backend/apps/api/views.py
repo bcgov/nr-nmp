926911bd6bee5be26d5f18936b10693be5e4f456
raarielgrace <raarielgrace@gmail.com>
2025-01-20 13:57:07 -0800

    feat: [NMP-144] CropTypes endpoint (#142)
    Co-authored-by: Dallas Richmond <dallas.richmond@gov.bc.ca>

--- backend/apps/api/views.py
+++ backend/apps/api/views.py
@@ -2,6 +2,19 @@ from rest_framework import status, viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from apps.crops.models import CropTypes
+from apps.crops.serializers import CropTypesSerializer
+
+class APIViewSet(viewsets.ViewSet):
+    @action(detail=True, methods=['get'])
+    def cropTypes(self, request, pk=None):
+        crop_types = CropTypes.objects.all()
+        serializer = CropTypesSerializer(crop_types, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+from rest_framework import status, viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from apps.crops.models import CropTypes
 from apps.crops.models import Crops
 from apps.crops.serializers import CropTypesSerializer
 from apps.crops.serializers import CropsSerializer
