2d00c7269fb194af597ea66c1235cec82614f9d4
raarielgrace <raarielgrace@gmail.com>
2025-01-20 13:57:07 -0800

    feat: [NMP-144] CropTypes endpoint (#142)
    Co-authored-by: Dallas Richmond <dallas.richmond@gov.bc.ca>

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,22 +1,15 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { SSOProvider } from '@bcgov/citz-imb-sso-react';
 import App from './App.tsx';
 import AppProvider from './providers/AppProvider.tsx';
-import { env } from '@/env';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <SSOProvider
-      backendURL={env.VITE_BACKEND_URL}
-      idpHint="idir"
-    >
-      <AppProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </AppProvider>
-    </SSOProvider>
+    <AppProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AppProvider>
   </StrictMode>,
 );
