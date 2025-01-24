2f5e4e6c810df8dd5a195308a8938ae6de3f9f38
raarielgrace <raarielgrace@gmail.com>
2025-01-24 15:31:01 -0800

    feat: [NR-NMP-111] "I have animals" radio buttons (#146)

--- backend/apps/animals/models.py
+++ backend/apps/animals/models.py
@@ -8,19 +8,3 @@ class Animals(models.Model):
     class Meta:
         managed = False
         db_table = 'animals'
-
-class AnimalSubtype(models.Model):
-    id = models.IntegerField(primary_key=True)
-    name = models.CharField(max_length=100)
-    liquidpergalperanimalperday = models.FloatField()
-    solidpergalperanimalperday = models.FloatField()
-    solidperpoundperanimalperday = models.FloatField()
-    solidliquidseparationpercentage = models.IntegerField()
-    washwater = models.FloatField()
-    milkproduction = models.FloatField()
-    animalid = models.IntegerField()
-    sortorder = models.IntegerField()
-
-    class Meta:
-        managed = False
-        db_table = 'animal_subtype'
