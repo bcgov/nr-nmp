6cfc209f5bcd486e21499d31d08d4c69469d2d2e
raarielgrace <raarielgrace@gmail.com>
2025-01-24 15:31:01 -0800

    feat: [NR-NMP-111] "I have animals" radio buttons (#146)

--- backend/apps/api/views.py
+++ backend/apps/api/views.py
@@ -12,16 +12,13 @@ from apps.animals.models import *
 from apps.animals.serializers import *
 
 class APIViewSet(viewsets.ViewSet):
+
     @action(detail=True, methods=['get'])
     def cropTypes(self, request, pk=None):
         crop_types = CropTypes.objects.all()
         serializer = CropTypesSerializer(crop_types, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
-    @action(detail=True, methods=['get'])
-    def crops(self, request, pk=None):
-        crops = Crops.objects.all()
-        serializer = CropsSerializer(crops, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+
     @action(detail=True, methods=['get'])
     def animals(self, request, pk=None):
         animals = Animals.objects.all()
