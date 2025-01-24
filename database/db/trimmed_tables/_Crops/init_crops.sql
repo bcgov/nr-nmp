cc0bd64b58a65a72fab8605750bdf363a80c2365
Dallas Richmond <dallas.richmond@gov.bc.ca>
2025-01-23 20:27:48 -0800

    WIP

--- database/db/trimmed_tables/_Crops/init_crops.sql
+++ database/db/trimmed_tables/_Crops/init_crops.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS crops (
   Id INT PRIMARY KEY,
   CropName VARCHAR(100) NOT NULL,
-  ADD CONSTRAINT fk_crop_type
-  FOREIGN KEY (CropTypeId) REFERENCES crop_types(id);  YieldCd FLOAT NOT NULL,
+  CropTypeId INT NOT NULL,
+  YieldCd FLOAT NOT NULL,
   CropRemovalFactorNitrogen FLOAT NOT NULL,
   CropRemovalFactorP2O5 FLOAT NOT NULL,
   CropRemovalFactorK2O FLOAT NOT NULL,
@@ -12,6 +12,8 @@ CREATE TABLE IF NOT EXISTS crops (
   PreviousCropCode INT NOT NULL,
   SortNumber INT NOT NULL,
   ManureApplicationHistory INT NOT NULL,
-  HarvestBushelsPerTon FLOAT
+  HarvestBushelsPerTon FLOAT,
+  CONSTRAINT fk_crop_type FOREIGN KEY (CropTypeId) REFERENCES crop_types(Id)
 );
+
 \copy crops (Id, CropName, CropTypeId, YieldCd, CropRemovalFactorNitrogen, CropRemovalFactorP2O5, CropRemovalFactorK2O, NitrogenRecommendationId, NitrogenRecommendationPoundPerAcre, NitrogenRecommendationUpperLimitPoundPerAcre, PreviousCropCode, SortNumber, ManureApplicationHistory, HarvestBushelsPerTon) from 'docker-entrypoint-initdb.d/_Crops_202501211005.csv' with header delimiter ',' CSV ;
