4a5c1b463854dfd5a769d025a6c2d9371f6c70eb
dallascrichmond <113049138+dallascrichmond@users.noreply.github.com>
2025-01-28 13:54:59 -0800

    feat: [NMP-125] Crops Tab (#148)
    Co-authored-by: Ariel Rotter-Aboyoun <raarielgrace@gmail.com>

--- frontend/src/context/APICacheContext.tsx
+++ frontend/src/context/APICacheContext.tsx
@@ -1,5 +1,5 @@
 import React, { createContext } from 'react';
-import APICache from '@/types/APICache';
+import APICache from '@/utils/APICache';
 
 const cache = new APICache();
 const APICacheContext = createContext(cache);
