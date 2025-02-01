34af29f703838ba4a326407a825031cd0a9eca2f
Dallas Richmond <dallas.richmond@gov.bc.ca>
2025-01-31 18:32:28 -0800

    Revert "Revert "Basic Django backend set up""
    This reverts commit 979db8a539891b7fba2e51e2956be3cd27ca38b7.

--- backend/apps/animals/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Animals
-
-class AnimalsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Animals
-        fields = '__all__'
