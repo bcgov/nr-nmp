885b602d00d94e4d167bd4ec251ca7824cadd94b
raarielgrace <raarielgrace@gmail.com>
2025-02-03 13:52:13 -0800

    feat: [NR-NMP-149] Added regions and subregions (#150)

--- backend/apps/animals/admin.py
+++ backend/apps/animals/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Animals
+from .models import *
 
 admin.site.register(Animals)
+admin.site.register(AnimalSubtype)
---
+++ backend/apps/shared/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import *
+
+class RegionsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Regions
+        fields = '__all__'
+
+class SubregionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Subregion
+        fields = '__all__'
