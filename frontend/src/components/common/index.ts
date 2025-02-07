e2ef39448f568c77582e1fb43c14cf7330e9d937
Ariel Rotter-Aboyoun <raarielgrace@gmail.com>
2025-02-07 11:32:34 -0800

    Dropdown renaming and default value

--- frontend/src/components/common/index.ts
+++ frontend/src/components/common/index.ts
@@ -4,7 +4,7 @@ import Footer from './Footer/Footer';
 import InputField from './InputField/InputField';
 import RadioButton from './RadioButton/RadioButton';
 import Checkbox from './Checkbox/Checkbox';
-import Dropdown from './DropDown/DropDown';
+import Dropdown from './Dropdown/Dropdown';
 import Card from './Card/Card';
 import Modal from './Modal/Modal';
 
