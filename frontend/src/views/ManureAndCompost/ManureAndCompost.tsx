c384d645a1b1a5f53265e7e08a3dd04f7f637fa3
dallascrichmond <113049138+dallascrichmond@users.noreply.github.com>
2025-02-13 11:14:48 -0800

    feat: Nmp 118 manure imports (#169)

--- frontend/src/views/ManureAndCompost/ManureAndCompost.tsx
+++ frontend/src/views/ManureAndCompost/ManureAndCompost.tsx
@@ -1,90 +1,48 @@
-/**
- * @summary The Field and Soil page for the application
- */
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import useAppService from '@/services/app/useAppService';
 import NMPFile from '@/types/NMPFile';
-import defaultNMPFile from '@/constants/DefaultNMPFile';
-import FieldList from './FieldList/FieldList';
-import SoilTests from './SoilTests/SoilTests';
-import Crops from './Crops/Crops';
+import NMPFileImportedManureData from '@/types/NMPFileImportedManureData';
 import { Card, Button } from '../../components/common';
 import { TabOptions, TabContentDisplay } from '../../components/common/Tabs/Tabs';
-import { CardHeader, Banner, ButtonWrapper } from './fieldAndSoil.styles';
-import NMPFileCropData from '@/types/NMPFileCropData';
-import blankNMPFileYearData from '@/constants/BlankNMPFileYearData';
+import { CardHeader, Banner, ButtonWrapper } from './manureAndCompost.styles';
+import ManureAndImports from './ManureAndImports/ManureAndImports';
+import NutrientAnalysis from './NutrientAnalysis/NutrientAnalysis';
 
-export default function FieldAndSoil() {
+export default function ManureAndCompost() {
   const { state, setNMPFile } = useAppService();
   const navigate = useNavigate();
   const [activeTab, setActiveTab] = useState(0);
-  const [fields, setFields] = useState<
-    {
-      FieldName: string;
-      Area: string;
-      PreviousYearManureApplicationFrequency: string;
-      Comment: string;
-      SoilTest: object;
-      Crops: NMPFileCropData[];
-    }[]
-  >([]);
+  const [manures, setManures] = useState<NMPFileImportedManureData[]>([]);
 
   const tabs = [
     {
-      id: 'field-list',
-      label: 'Field List',
-      content: (
-        <FieldList
-          fields={fields}
-          setFields={setFields}
-        />
-      ),
+      id: 'manure-imports',
+      label: 'Manure and Imports',
+      content: <ManureAndImports />,
     },
     {
-      id: 'soil-test',
-      label: 'Soil Tests',
-      content: (
-        <SoilTests
-          fields={fields}
-          setFields={setFields}
-        />
-      ),
-    },
-    {
-      id: 'crops',
-      label: 'Crops',
-      content: (
-        <Crops
-          fields={fields}
-          setFields={setFields}
-        />
-      ),
+      id: 'nutrient-analysis',
+      label: 'Nutrient Analysis',
+      content: <NutrientAnalysis />,
     },
   ];
 
   const handleNext = () => {
-    let nmpFile: NMPFile;
-    if (state.nmpFile) nmpFile = JSON.parse(state.nmpFile);
-    else {
-      nmpFile = { ...defaultNMPFile };
-      nmpFile.years.push({ ...blankNMPFileYearData });
+    let nmpFile: NMPFile | null = null;
+    if (state.nmpFile) {
+      nmpFile = JSON.parse(state.nmpFile);
     }
-    if (nmpFile.years.length > 0) {
-      nmpFile.years[0].Fields = fields.map((field) => ({
-        FieldName: field.FieldName,
-        Area: parseFloat(field.Area),
-        PreviousYearManureApplicationFrequency: field.PreviousYearManureApplicationFrequency,
-        Comment: field.Comment,
-        SoilTest: field.SoilTest,
-        Crops: field.Crops,
+    if (nmpFile && nmpFile.years.length > 0 && manures.length > 0) {
+      nmpFile.years[0].ImportedManures = manures.map((manure) => ({
+        ...manure,
       }));
     }
     setNMPFile(JSON.stringify(nmpFile));
 
     // if on the last tab navigate to calculate nutrients page
     if (activeTab === tabs.length - 1) {
-      navigate('/manure-and-compost');
+      navigate('/calculate-nutrients');
     } else {
       setActiveTab(activeTab + 1);
     }
@@ -92,14 +50,14 @@ export default function FieldAndSoil() {
 
   const handlePrevious = () => {
     if (activeTab > 0) setActiveTab(activeTab - 1);
-    else navigate('/farm-information');
+    else navigate('/field-and-soil');
   };
 
   // assumes only 1 year, edit
   useEffect(() => {
     if (state.nmpFile) {
       const parsedData = JSON.parse(state.nmpFile);
-      setFields(parsedData.years[0].Fields);
+      setManures(parsedData.years[0].Fields);
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [state]);
@@ -130,7 +88,7 @@ export default function FieldAndSoil() {
           handleClick={handleNext}
           aria-label="Next"
           variant="primary"
-          disabled={fields.length === 0}
+          disabled={false}
         />
       </ButtonWrapper>
       <ButtonWrapper position="left">
