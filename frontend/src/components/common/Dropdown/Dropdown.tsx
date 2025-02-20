cb7e832f432a23a423e75ede0cce87d7856b9f6d
lunamoonmoon <teal0521@gmail.com>
2025-02-20 09:41:17 -0800

    Modal showing with manure types

--- frontend/src/components/common/Dropdown/Dropdown.tsx
+++ frontend/src/components/common/Dropdown/Dropdown.tsx
@@ -8,7 +8,7 @@ interface DropdownProps {
   label: string;
   name: string;
   value: number | string;
-  options: { value: number; label: string }[];
+  options: { value: number | undefined; label: string | undefined }[];
   onChange: (e: React.ChangeEvent<HTMLSelectElement>) => void;
   flex?: string;
 }
--- frontend/src/views/ManureAndCompost/NutrientAnalysis/NutrientAnalysis.tsx
+++ frontend/src/views/ManureAndCompost/NutrientAnalysis/NutrientAnalysis.tsx
@@ -2,11 +2,12 @@
  * @summary The nutrient analysis tab on the manure page for the application
  */
 
-import { useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEdit, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { APICacheContext } from '@/context/APICacheContext';
 import NMPFileImportedManureData from '@/types/NMPFileImportedManureData';
-import { Dropdown, InputField, Button, Modal, RadioButton } from '../../../components/common';
+import { Button, Modal } from '../../../components/common';
 import {
   ContentWrapper,
   Column,
@@ -16,48 +17,83 @@ import {
   Header,
   ButtonWrapper,
 } from './nutrientAnalsysis.styles';
-import { ModalContent } from '@/components/common/Modal/modal.styles';
-import { DropdownWrapper } from '@/components/common/Dropdown/dropdown.styles';
-import { RadioButtonWrapper } from '@/components/common/RadioButton/radioButton.styles';
+import AnalysisModal from './AnalysisModal/AnalysisModal';
 
 interface ManureListProps {
   manures: NMPFileImportedManureData[];
 }
 
-interface NutrientAnalysisForm {
-  ManureName: string;
-  MaterialType: string;
-  BookLab: string;
-  MaterialName: string;
-  Nutrients: { Moisture: number; N: number; P: number; K: number };
-}
-
 export default function NutrientAnalysis({ manures }: ManureListProps) {
+  const apiCache = useContext(APICacheContext);
   const [isModalVisible, setIsModalVisible] = useState(false);
-  const [editIndex, setEditIndex] = useState<number | null>(null);
-  const [nutrientAnalysisFormData, setNutrientAnalysisFormData] = useState<NutrientAnalysisForm[]>(
-    [],
-  );
+  // manure data from db
+  const [manureData, setManureData] = useState<NMPFileImportedManureData[]>([]);
+  // an array of objects to hold nutrient analysis form data
+  // for each manuresource user can create nutrient analysis' objects
+  const [nutrientAnalysisFormData, setNutrientAnalysisFormData] = useState<
+    {
+      ManureSource: string;
+      ManureName: string;
+      MaterialType: string;
+      BookLab: string;
+      MaterialName: string;
+      Nutrients: { Moisture: number; N: number; NH4N: number; P: number; K: number };
+    }[]
+  >([
+    {
+      ManureSource: '',
+      ManureName: '',
+      MaterialType: '',
+      BookLab: '',
+      MaterialName: '',
+      Nutrients: { Moisture: 0, N: 0, NH4N: 0, P: 0, K: 0 },
+    },
+  ]);
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
-    const { name, value } = e.target;
-    setNutrientAnalysisFormData({ ...nutrientAnalysisFormData, [name]: value });
-  };
+  // add new nutrient analysis, open modal
+  // const handleAdd = () => {
+  //   setIsModalVisible(true);
+  //   setAnalysisIndex(nutrientAnalysisFormData.length);
+  //   setNutrientAnalysisFormData((prevData) => [
+  //     ...prevData,
+  //     {
+  //       ManureSource: '',
+  //       ManureName: '',
+  //       MaterialType: '',
+  //       BookLab: '',
+  //       MaterialName: '',
+  //       Nutrients: { Moisture: 0, N: 0, NH4N: 0, P: 0, K: 0 },
+  //     },
+  //   ]);
+  // };
 
-  const handleEdit = (index: number) => {
-    setNutrientAnalysisFormData(nutrientAnalysisFormData[index]); // Fix here
-    setEditIndex(index);
+  // how to handle radio buttons?
+  // const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+  //   const { name, value } = e.target;
+  //   setNutrientAnalysisFormData({ ...nutrientAnalysisFormData, [name]: value });
+  // };
+
+  const handleEdit = () => {
     setIsModalVisible(true);
   };
 
-  const handleDelete = (index: number) => {
-    const updatedAnalysis = nutrientAnalysisFormData.filter((_, i) => i !== index);
-    setNutrientAnalysisFormData(updatedAnalysis);
+  const handleDelete = () => {
+    setNutrientAnalysisFormData(nutrientAnalysisFormData);
   };
 
   const handleSubmit = () => {
     setIsModalVisible(false);
   };
+
+  // get manure types
+  useEffect(() => {
+    apiCache.callEndpoint('api/manures/').then((response: { status?: any; data: any }) => {
+      if (response.status === 200) {
+        const { data } = response;
+        setManureData(data);
+      }
+    });
+  }, [apiCache]);
 
   return (
     <div>
@@ -69,6 +105,7 @@ export default function NutrientAnalysis({ manures }: ManureListProps) {
             <Column>Material Type</Column>
             <Column>Moisture(%)</Column>
             <Column>N(%)</Column>
+            <Column>NH4N(%)</Column>
             <Column>P(%)</Column>
             <Column>K(%)</Column>
             <Column align="right">Actions</Column>
@@ -79,18 +116,19 @@ export default function NutrientAnalysis({ manures }: ManureListProps) {
             <ListItem>{NAnalysis.MaterialType}</ListItem>
             <ListItem>{NAnalysis.Nutrients.Moisture}</ListItem>
             <ListItem>{NAnalysis.Nutrients.N}</ListItem>
+            <ListItem>{NAnalysis.Nutrients.NH4N}</ListItem>
             <ListItem>{NAnalysis.Nutrients.P}</ListItem>
             <ListItem>{NAnalysis.Nutrients.K}</ListItem>
             <ListItem align="right">
               <button
                 type="button"
-                onClick={() => handleEdit(index)}
+                onClick={() => handleEdit()}
               >
                 <FontAwesomeIcon icon={faEdit} />
               </button>
               <button
                 type="button"
-                onClick={() => handleDelete(index)}
+                onClick={() => handleDelete()}
               >
                 <FontAwesomeIcon icon={faTrash} />
               </button>
@@ -98,16 +136,19 @@ export default function NutrientAnalysis({ manures }: ManureListProps) {
           </ListItemContainer>
         ))}
       </ContentWrapper>
-      <ButtonContainer>
-        {/* button to add a nutrient analysis if there are manures to add it to */}
-        <Button
-          variant="default"
-          size="sm"
-          disabled={manures.length === 0}
-          text="Add a Nutrient Analysis"
-          handleClick={() => setIsModalVisible(true)}
-        />
-      </ButtonContainer>
+      {manures.length > 0 && (
+        <ButtonContainer>
+          {/* button to add a nutrient analysis if there are manures to add it to */}
+          {/* add a new nutrient analysis */}
+          <Button
+            variant="default"
+            size="sm"
+            disabled={manures.length === 0}
+            text="Add a Nutrient Analysis"
+            handleClick={() => setIsModalVisible(true)}
+          />
+        </ButtonContainer>
+      )}
       <Modal
         isVisible={isModalVisible}
         onClose={() => setIsModalVisible(false)}
@@ -137,223 +178,11 @@ export default function NutrientAnalysis({ manures }: ManureListProps) {
           </>
         }
       >
-        <div />
-        <DropdownWrapper>
-          {/* // modal has "Source of Material" dropdown which maps manures input */}
-          {/* <Dropdown
-            label="Source of Material"
-            name="Source of Material"
-            value={nutrientAnalysisFormData.MaterialType}
-            options={[]}
-            onChange={handleChange}
-          /> */}
-          {/* // material type dropdown is this a db to get? */}
-          <Dropdown
-            label="Material Type"
-            name="MaterialType"
-            value={nutrientAnalysisFormData.MaterialType}
-            options={[]}
-            onChange={handleChange}
-          />
-        </DropdownWrapper>
-        <RadioButtonWrapper>
-          {/* // radio button "Book Value" and "Lab Analysis" auto sleected based on material type */}
-          <RadioButton
-            label={''}
-            name={''}
-            value={''}
-            checked={false}
-            onChange={handleChange}
-          />
-        </RadioButtonWrapper>
-        {/* 
-          // Book value
-
-          // Lab Analysis
-          // material name Custom - material tye here
-          // turns nutrient values into inputs
-
-          // Moisture, N, NH4-N, P, K
-          // N03-N for lab analysis
-        */}
+        <AnalysisModal
+          manures={manures}
+          manureData={manureData}
+        />
       </Modal>
     </div>
   );
 }
-
-// const initialFieldFormData = {
-//   FieldName: '',
-//   Area: '',
-//   PreviousYearManureApplicationFrequency: '0',
-//   Comment: '',
-//   SoilTest: {},
-//   Crops: [],
-// };
-
-// export default function FieldList({ fields, setFields }: FieldListProps) {
-//   const [isModalVisible, setIsModalVisible] = useState(false);
-//   const [editIndex, setEditIndex] = useState<number | null>(null);
-//   const [fieldFormData, setFieldFormData] = useState<NMPFileFieldData>(initialFieldFormData);
-//   const [errors, setErrors] = useState<{ [key: string]: string }>({});
-
-//   const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
-//     const { name, value } = e.target;
-//     setFieldFormData({ ...fieldFormData, [name]: value });
-//   };
-
-
-//   const validate = () => {
-//     const newErrors: { [key: string]: string } = {};
-//     if (!fieldFormData.FieldName.trim()) {
-//       newErrors.FieldName = 'Field Name is required';
-//     } else if (
-//       fields.some(
-//         (field, index) =>
-//           field.FieldName.trim().toLowerCase() === fieldFormData.FieldName.trim().toLowerCase() &&
-//           index !== editIndex,
-//       )
-//     ) {
-//       newErrors.FieldName = 'Field Name must be unique';
-//     }
-//     if (!fieldFormData.Area.trim() || Number.isNaN(Number(fieldFormData.Area))) {
-//       newErrors.Area = 'Area is required and must be a number';
-//     }
-//     if (fieldFormData.PreviousYearManureApplicationFrequency === '0') {
-//       newErrors.PreviousYearManureApplicationFrequency =
-//         'Please select a manure application frequency';
-//     }
-//     return newErrors;
-//   };
-
-//   const handleSubmit = () => {
-//     const validationErrors = validate();
-//     if (Object.keys(validationErrors).length > 0) {
-//       setErrors(validationErrors);
-//       return;
-//     }
-//     setErrors({});
-//     if (editIndex !== null) {
-//       const updatedFields = fields.map((field, index) =>
-//         index === editIndex ? fieldFormData : field,
-//       );
-//       setFields(updatedFields);
-//       setEditIndex(null);
-//     } else {
-//       setFields([...fields, fieldFormData]);
-//     }
-//     setFieldFormData(initialFieldFormData);
-//     setIsModalVisible(false);
-//   };
-
-//   const filteredFields = fields.filter((field) => field.FieldName.trim() !== '');
-
-//   return (
-//     <div>
-//       <ButtonContainer hasFields={filteredFields.length > 0}>
-//         <Button
-//           text="Add Field"
-//           handleClick={() => setIsModalVisible(true)}
-//           aria-label="Add Field"
-//           variant="primary"
-//           size="sm"
-//           disabled={false}
-//         />
-//       </ButtonContainer>
-//       <ContentWrapper hasFields={filteredFields.length > 0}>
-//         {filteredFields.length > 0 && (
-//           <Header>
-//             <Column>Field Name</Column>
-//             <Column>Area</Column>
-//             <Column>Comments</Column>
-//             <Column align="right">Actions</Column>
-//           </Header>
-//         )}
-//         {filteredFields.map((field, index) => (
-//           <ListItemContainer key={field.FieldName}>
-//             <ListItem>{field.FieldName}</ListItem>
-//             <ListItem>{field.Area}</ListItem>
-//             <ListItem>{field.Comment}</ListItem>
-//             <ListItem align="right">
-//               <button
-//                 type="button"
-//                 onClick={() => handleEdit(index)}
-//               >
-//                 <FontAwesomeIcon icon={faEdit} />
-//               </button>
-//               <button
-//                 type="button"
-//                 onClick={() => handleDelete(index)}
-//               >
-//                 <FontAwesomeIcon icon={faTrash} />
-//               </button>
-//             </ListItem>
-//           </ListItemContainer>
-//         ))}
-//       </ContentWrapper>
-//       <Modal
-//         isVisible={isModalVisible}
-//         title={editIndex !== null ? 'Edit Field' : 'Add Field'}
-//         onClose={() => setIsModalVisible(false)}
-//         footer={
-//           <>
-//             <ButtonWrapper>
-//               <Button
-//                 text="Cancel"
-//                 handleClick={() => setIsModalVisible(false)}
-//                 aria-label="Cancel"
-//                 variant="secondary"
-//                 size="sm"
-//                 disabled={false}
-//               />
-//             </ButtonWrapper>
-//             <ButtonWrapper>
-//               <Button
-//                 text="Submit"
-//                 handleClick={handleSubmit}
-//                 aria-label="Submit"
-//                 variant="primary"
-//                 size="sm"
-//                 disabled={false}
-//               />
-//             </ButtonWrapper>
-//           </>
-//         }
-//       >
-//         {errors.FieldName && <ErrorText>{errors.FieldName}</ErrorText>}
-//         <InputField
-//           label="Field Name"
-//           type="text"
-//           name="FieldName"
-//           value={fieldFormData.FieldName}
-//           onChange={handleChange}
-//         />
-//         {errors.Area && <ErrorText>{errors.Area}</ErrorText>}
-//         <InputField
-//           label="Area"
-//           type="text"
-//           name="Area"
-//           value={fieldFormData.Area}
-//           onChange={handleChange}
-//         />
-//         {errors.PreviousYearManureApplicationFrequency && (
-//           <ErrorText>{errors.PreviousYearManureApplicationFrequency}</ErrorText>
-//         )}
-//         <Dropdown
-//           label="Manure application in previous years"
-//           name="PreviousYearManureApplicationFrequency"
-//           value={fieldFormData.PreviousYearManureApplicationFrequency}
-//           options={manureOptions}
-//           onChange={handleChange}
-//         />
-//         <InputField
-//           label="Comments (optional)"
-//           type="text"
-//           name="Comment"
-//           value={fieldFormData.Comment}
-//           onChange={handleChange}
-//         />
-//       </Modal>
-//     </div>
-//   );
-// }
-
