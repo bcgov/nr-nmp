ef324afe333123549da770d9c32f569bc5ad05d3
Dallas Richmond <dallas.richmond@gov.bc.ca>
2025-03-03 11:52:58 -0800

    Testing adding new model

--- backend/crops/models.py
+++ backend/crops/models.py
@@ -6,3 +6,8 @@ class CropTypes(models.Model):
     crudeproteinrequired = models.BooleanField()
     customcrop = models.BooleanField()
     modifynitrogen = models.BooleanField()
+
+class Crops(models.Model):
+    name = models.CharField(max_length=100)
+    crop_type = models.ForeignKey(CropTypes, on_delete=models.CASCADE)
+    nitrogen = models.FloatField()
