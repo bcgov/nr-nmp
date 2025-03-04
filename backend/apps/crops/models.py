b7beb4fd102a02c9941a526b9d8118dda0dc27e6
Paul Garewal <paul.garewal@gov.bc.ca>
2025-03-04 14:25:20 -0800

    removed YieldFactor and tested migration successfully

--- backend/apps/crops/models.py
+++ backend/apps/crops/models.py
@@ -31,15 +31,3 @@ class Crops(models.Model):
     class Meta:
         managed = True
         db_table = 'crops'
-
-# New model that will be managed by Django ORM
-class YieldFactor(models.Model):
-    crop = models.ForeignKey(Crops, on_delete=models.CASCADE, related_name='yield_factors')
-    region = models.CharField(max_length=100)
-    yield_value = models.FloatField(help_text="Yield factor value for the crop in this region")
-    unit = models.CharField(max_length=50)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    
-    def __str__(self):
-        return f"{self.crop.cropname} - {self.region}: {self.yield_value} {self.unit}"
