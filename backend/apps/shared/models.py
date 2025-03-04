5c50e26b9ccd9a8e787971eaff8587a7c3cd1621
Paul Garewal <paul.garewal@gov.bc.ca>
2025-03-04 14:21:53 -0800

    removed test migration and seed data test

--- backend/apps/shared/models.py
+++ backend/apps/shared/models.py
@@ -22,13 +22,3 @@ class Subregion(models.Model):
     class Meta:
         managed = True
         db_table = 'subregion'
-
-# New model that will be managed by Django ORM
-class Note(models.Model):
-    title = models.CharField(max_length=200)
-    content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    
-    def __str__(self):
-        return self.title
