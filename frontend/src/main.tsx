df20c97c76f1b43aecc78d6b3f12b29a1ba42477
Ariel Rotter-Aboyoun <raarielgrace@gmail.com>
2025-03-07 07:31:48 -0800

    fix: [NR-NMP-194] Removed StrictMode

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,4 +1,3 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx';
@@ -6,13 +5,11 @@ import AppProvider from './context/AppProvider.tsx';
 import { APICacheProvider } from './context/APICacheContext.tsx';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <AppProvider>
-      <APICacheProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </APICacheProvider>
-    </AppProvider>
-  </StrictMode>,
+  <AppProvider>
+    <APICacheProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </APICacheProvider>
+  </AppProvider>,
 );
