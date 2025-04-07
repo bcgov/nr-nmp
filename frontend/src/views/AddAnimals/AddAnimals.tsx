0cc866b91bafcfb1a393640d2574e28e36045603
lunamoonmoon <teal0521@gmail.com>
2025-04-07 15:10:54 -0700

    added date field and column

--- frontend/src/views/AddAnimals/AddAnimals.tsx
+++ frontend/src/views/AddAnimals/AddAnimals.tsx
@@ -31,6 +31,7 @@ export default function AddAnimals() {
   const [animalOptions, setAnimalOptions] = useState<{ value: number; label: string }[]>([]);
   const [selectedAnimal, setSelectedAnimal] = useState<string | null>(null);
   const [nextDisabled, setNextDisabled] = useState(false);
+  const [isAllSelected, setIsAllSelected] = useState(false);
 
   const [formData, setFormData] = useState<(AnimalData | null)[]>([]);
   const [elems, setElems] = useState<(React.ReactNode | null)[]>([]);
@@ -69,6 +70,12 @@ export default function AddAnimals() {
       next[index] = null;
       return next;
     });
+  };
+
+  const handleSelectAll = () => {
+    const newIsAllSelected = !isAllSelected;
+    setIsAllSelected(newIsAllSelected);
+    // setSelectedRows(new Array(elems.length).fill(newIsAllSelected));
   };
 
   // Init data & elems on first render
