9e336ef8f97ecba3c953efd8a4ab459753ab10c3
lunamoonmoon <teal0521@gmail.com>
2025-04-11 15:06:48 -0700

    grid and add updated

--- frontend/src/views/AddAnimals/DairyCattle/DairyCattle.tsx
+++ frontend/src/views/AddAnimals/DairyCattle/DairyCattle.tsx
@@ -29,7 +29,6 @@ export default function DairyCattle({ onChange }: { onChange: (data: DairyCattle
   const [subtypeOptions, setSubtypeOptions] = useState<{ value: number; label: string }[]>([]);
   const [breeds, setBreeds] = useState<DairyCattleBreed[]>([]);
   const [breedOptions, setBreedOptions] = useState<{ value: number; label: string }[]>([]);
-  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
   // Initial values for milking fields, if "Milking cow" is selected //
   const washWaterInit = useMemo(() => {
