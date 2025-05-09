232c05d7faa2ceda98a78567e0e8b11600cea787
Wilson Wong <wilwong89@gmail.com>
2025-05-08 18:24:16 -0700

    Extract and componentize modal for adding animals

--- frontend/src/views/AddAnimals/AddAnimals.tsx
+++ frontend/src/views/AddAnimals/AddAnimals.tsx
@@ -2,45 +2,21 @@
 /**
  * @summary This is the Add Animal list Tab
  */
-import React, { FormEvent, useCallback, useEffect, useMemo, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEdit, faTrash } from '@fortawesome/free-solid-svg-icons';
 import { DataGrid, GridColDef } from '@mui/x-data-grid';
-import Divider from '@mui/material/Divider';
-import Grid from '@mui/material/Grid';
-import {
-  Button,
-  ButtonGroup,
-  Dialog,
-  Modal,
-  Form,
-  Select,
-} from '@bcgov/design-system-react-components';
-import {
-  formCss,
-  customTableStyle,
-  tableActionButtonCss,
-  addRecordGroupStyle,
-  modalHeaderStyle,
-  modalPaddingStyle,
-} from '../../common.styles';
+import { Button, ButtonGroup } from '@bcgov/design-system-react-components';
+import { customTableStyle, tableActionButtonCss, addRecordGroupStyle } from '../../common.styles';
 import useAppService from '@/services/app/useAppService';
 import { AppTitle, PageTitle, TabsMaterial } from '@/components/common';
-import {
-  AnimalData,
-  BeefCattleData,
-  DairyCattleData,
-  initialBeefFormData,
-  initialDairyFormData,
-  initialEmptyData,
-} from './types';
-import BeefCattle from './BeefCattle';
-import DairyCattle from './DairyCattle/DairyCattle';
+import { AnimalData, initialBeefFormData, initialDairyFormData, initialEmptyData } from './types';
 import { FARM_INFORMATION, MANURE_IMPORTS } from '@/constants/RouteConstants';
 import ProgressStepper from '@/components/common/ProgressStepper/ProgressStepper';
 import { initAnimals, saveAnimalsToFile } from './utils';
 import { ErrorText, StyledContent } from './addAnimals.styles';
+import ModalComponent from './ModalComponent';
 
 // need a row id
 type tempAnimalData = AnimalData & { id?: string };
@@ -51,8 +27,6 @@ export default function AddAnimals() {
   const [isEditingForm, setIsEditingForm] = useState<boolean>(false);
   const [isDialogOpen, setIsDialogOpen] = useState<boolean>(false);
   const [showViewError, setShowViewError] = useState<string>('');
-  // array of all livestck entered by the user so far
-  const [animalForm, setAnimalForm] = useState<React.ReactNode | null>(null);
 
   const navigate = useNavigate();
 
@@ -64,57 +38,26 @@ export default function AddAnimals() {
     })),
   );
 
-  const animalOptions = [
-    { id: '1', label: 'Beef Cattle' },
-    { id: '2', label: 'Dairy Cattle' },
-  ];
-
   useEffect(() => {
     setShowAnimalsStep(true);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  // // onSubmit saves to animalList which saves to nmpFile when user clicks next
-  const onSubmit = (e: FormEvent<HTMLFormElement>) => {
-    // Prevent default browser page refresh.
-    e.preventDefault();
-
+  const handleSubmit = (newFormData: tempAnimalData) => {
     if (isEditingForm) {
       // If editing, find and replace field instead of adding new field
       const replaceIndex = animalList.findIndex((element) => element?.id === formData?.id);
       setAnimalList((prev) => {
         const newList = [...prev];
-        newList[replaceIndex] = { ...formData };
+        newList[replaceIndex] = { ...newFormData };
         return newList;
       });
     } else {
-      setAnimalList((prev) => [...prev, { ...formData, id: prev?.length.toString() ?? '0' }]);
+      setAnimalList((prev) => [...prev, { ...newFormData, id: prev?.length.toString() ?? '0' }]);
     }
     handleDialogClose();
     setShowViewError('');
   };
-
-  // sets the animal form for the modal
-  const handleAnimalType = useCallback(
-    (animal: string) => {
-      const newForm =
-        animal === '1' ? (
-          <BeefCattle
-            key={animalList.length}
-            formData={formData as BeefCattleData}
-            setFormData={setFormData as React.Dispatch<React.SetStateAction<BeefCattleData>>}
-          />
-        ) : (
-          <DairyCattle
-            key={animalList.length}
-            formData={formData as DairyCattleData}
-            setFormData={setFormData as React.Dispatch<React.SetStateAction<DairyCattleData>>}
-          />
-        );
-      setAnimalForm(newForm);
-    },
-    [animalList.length, formData],
-  );
 
   const handleEditRow = React.useCallback(
     (e: { row: AnimalData }) => {
@@ -127,13 +70,6 @@ export default function AddAnimals() {
           ...e.row,
         };
         setFormData(newFormData);
-        setAnimalForm(
-          <BeefCattle
-            key={animalList.length}
-            formData={newFormData as BeefCattleData}
-            setFormData={setFormData as React.Dispatch<React.SetStateAction<BeefCattleData>>}
-          />,
-        );
       }
       if (e.row.animalId === '2') {
         const newFormData = {
@@ -141,13 +77,6 @@ export default function AddAnimals() {
           ...e.row,
         };
         setFormData(newFormData);
-        setAnimalForm(
-          <DairyCattle
-            key={animalList.length}
-            formData={newFormData as DairyCattleData}
-            setFormData={setFormData as React.Dispatch<React.SetStateAction<DairyCattleData>>}
-          />,
-        );
       }
     },
     [animalList.length],
@@ -165,7 +94,6 @@ export default function AddAnimals() {
     setIsDialogOpen(false);
     setIsEditingForm(false);
     setFormData(initialEmptyData);
-    setAnimalForm(null);
   };
 
   const handleNextPage = () => {
@@ -267,82 +195,16 @@ export default function AddAnimals() {
             </Button>
           </ButtonGroup>
         </div>
-        <Modal
-          isDismissable
+        <ModalComponent
+          key={isDialogOpen.toString()}
+          initialModalData={formData}
+          handleDialogClose={handleDialogClose}
+          handleSubmit={handleSubmit}
           isOpen={isDialogOpen}
           onOpenChange={handleDialogClose}
+          isDismissable
           style={{ width: '700px' }}
-        >
-          <Dialog
-            isCloseable
-            role="dialog"
-            aria-labelledby="add-animal-dialog"
-          >
-            <div css={modalPaddingStyle}>
-              <span css={modalHeaderStyle}>{isEditingForm ? 'Edit animal' : 'Add animal'}</span>
-              <Divider
-                aria-hidden="true"
-                component="div"
-                css={{ marginTop: '0.5rem', marginBottom: '0.5rem' }}
-              />
-              <Form
-                css={formCss}
-                onSubmit={onSubmit}
-              >
-                <Grid>
-                  <Grid size={6}>
-                    {/* dynamically renders the form fields depending on the users choice of animal in the select below */}
-                    <Select
-                      isRequired
-                      name="animalId"
-                      items={animalOptions}
-                      label="Animal Type"
-                      placeholder="Select Animal Type"
-                      selectedKey={formData.animalId}
-                      onSelectionChange={(e: any) => {
-                        const selectedItem = animalOptions.find((item) => item.id === e);
-
-                        if (selectedItem) {
-                          setFormData(
-                            selectedItem.id === '1' ? initialBeefFormData : initialDairyFormData,
-                          );
-                          handleAnimalType(selectedItem.id);
-                        }
-                      }}
-                    />
-                    <div style={{ marginTop: '0.5rem' }}>{animalForm}</div>
-                  </Grid>
-                </Grid>
-                <Divider
-                  aria-hidden="true"
-                  component="div"
-                  css={{ marginTop: '1rem', marginBottom: '1rem' }}
-                />
-                <ButtonGroup
-                  alignment="end"
-                  orientation="horizontal"
-                >
-                  <Button
-                    type="reset"
-                    variant="secondary"
-                    onPress={handleDialogClose}
-                    aria-label="reset"
-                  >
-                    Cancel
-                  </Button>
-                  {/* can we use the components save function? */}
-                  <Button
-                    type="submit"
-                    variant="primary"
-                    aria-label="submit"
-                  >
-                    Confirm
-                  </Button>
-                </ButtonGroup>
-              </Form>
-            </div>
-          </Dialog>
-        </Modal>
+        />
       </>
       <TabsMaterial
         activeTab={0}
