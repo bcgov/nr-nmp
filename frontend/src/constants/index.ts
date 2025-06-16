dc7532b15e134b7906b4a9bbedb4bf68463f49ec
Ariel Rotter-Aboyoun <raarielgrace@gmail.com>
2025-06-16 13:39:52 -0700

    ugh

--- frontend/src/constants/index.ts
+++ frontend/src/constants/index.ts
@@ -4,7 +4,7 @@ import DefaultManureFormData from './DefaultManureFormData';
 import DEFAULT_NMPFILE from './DefaultNMPFile';
 import DEFAULT_NMPFILE_CROPS from './DefaultNMPFileCropsData';
 import DefaultSolidManureConversionFactors from './DefaultSolidManureConversionFactors';
-import { EMPTY_CROP_NUTRIENTS, EMPTY_NUTRIENT_COLUMNS } from './calculateNutrients';
+import { EMPTY_CROP_NUTRIENTS, EMPTY_NUTRIENT_COLUMNS } from './CalculateNutrients';
 import { INITIAL_BEEF_FORM_DATA, INITIAL_DAIRY_FORM_DATA } from './animals';
 import NMP_FILE_KEY from './context';
 
