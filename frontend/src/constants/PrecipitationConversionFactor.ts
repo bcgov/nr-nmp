f53d78d86bba8d1ead4078a538c21df166ab8831
Ariel Rotter-Aboyoun <raarielgrace@gmail.com>
2025-07-28 16:22:50 -0700

    Fix conversion factor layout

--- frontend/src/constants/PrecipitationConversionFactor.ts
+++ frontend/src/constants/PrecipitationConversionFactor.ts
@@ -1,7 +1,7 @@
 // eslint-disable-next-line no-shadow
 const enum PrecipitationConversionFactor {
-  Liquid = 0.000102408,
-  Solid = 0.024542388,
+  Liquid = 0.024542388,
+  Solid = 0.000102408,
 }
 
 export default PrecipitationConversionFactor;
