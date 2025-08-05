366f42c38e06485ad704ab76a96eefe6b3491dca
lunamoonmoon <5862lunamoon@gmail.com>
2025-08-05 10:05:00 -0700

    auto subtype for other animals and options for swine

--- frontend/src/views/AddAnimals/AnimalFormComponents/OtherAnimals.tsx
+++ frontend/src/views/AddAnimals/AnimalFormComponents/OtherAnimals.tsx
@@ -82,7 +82,7 @@ export default function OtherAnimals({
       }
     });
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [formData.animalId]);
 
   return (
     <AnimalFormWrapper
