19596de8cf1783b8c70d13dfb21330826dcf7a8b
lunamoonmoon <5862lunamoon@gmail.com>
2025-08-05 15:18:03 -0700

    Revert "Used SelectOption to reduce subtype complexity"
    This reverts commit eecb95c1d549ec67b410ad91f704722953188dad.

--- frontend/src/views/AddAnimals/AnimalFormComponents/OtherAnimals.tsx
+++ frontend/src/views/AddAnimals/AnimalFormComponents/OtherAnimals.tsx
@@ -33,7 +33,8 @@ export default function OtherAnimals({
 }: OtherAnimalsProps) {
   const apiCache = useContext(APICacheContext);
   const [showCollectionDays, setShowCollectionDays] = useState<boolean>(!!formData.daysCollected);
-  const [subtypes, setSubtypes] = useState<SelectOption<Subtype>[]>([]);
+  const [subtypes, setSubtypes] = useState<Subtype[]>([]);
+  const [subtypeOptions, setSubtypeOptions] = useState<{ id: string; label: string }[]>([]);
 
   const onSubmit = () => {
     // Calculate manure
@@ -44,9 +45,9 @@ export default function OtherAnimals({
         withManureCalc = {
           ...formData,
           manureData: {
-            name: selectedSubtype.value.name,
+            name: selectedSubtype.name,
             annualLiquidManure: calculateAnnualLiquidManure(
-              selectedSubtype.value.liquidpergalperanimalperday,
+              selectedSubtype.liquidpergalperanimalperday,
               formData.animalsPerFarm!,
               formData.daysCollected || 0,
             ),
@@ -57,10 +58,10 @@ export default function OtherAnimals({
         withManureCalc = {
           ...formData,
           manureData: {
-            name: selectedSubtype.value.name,
+            name: selectedSubtype.name,
             annualLiquidManure: undefined,
             annualSolidManure: calculateAnnualSolidManure(
-              selectedSubtype.value.solidperpoundperanimalperday,
+              selectedSubtype.solidperpoundperanimalperday,
               formData.animalsPerFarm!,
               formData.daysCollected || 0,
             ),
@@ -77,20 +78,22 @@ export default function OtherAnimals({
   useEffect(() => {
     apiCache.callEndpoint(`api/animal_subtypes/${formData.animalId}/`).then((response) => {
       if (response.status === 200) {
-        setSubtypes(
-          response.map((row: Subtype) => ({
-            id: row.id,
-            label: row.name,
-            value: {
-              name: row.name,
-              solidperpoundperanimalperday: row.solidperpoundperanimalperday,
-              liquidpergalperanimalperday: row.liquidpergalperanimalperday,
-            },
-          })),
-        );
+        const { data } = response;
+        const subtypez: Subtype[] = (data as Subtype[]).map((row: Subtype) => ({
+          id: row.id,
+          name: row.name,
+          solidperpoundperanimalperday: row.solidperpoundperanimalperday,
+          liquidpergalperanimalperday: row.liquidpergalperanimalperday,
+        }));
+        setSubtypes(data);
+        const subtypeOptionz: { id: string; label: string }[] = subtypez.map((row) => ({
+          id: row.id.toString(),
+          label: row.name,
+        }));
+        setSubtypeOptions(subtypeOptionz);
         // if animal not swine and only has one subtype option set subtype automatically
-        if (subtypes.length < 2) {
-          handleInputChanges({ subtype: String(subtypes[0].id) });
+        if (subtypeOptionz.length < 2) {
+          handleInputChanges({ subtype: String(subtypeOptionz[0].id) });
         }
       }
     });
@@ -111,7 +114,7 @@ export default function OtherAnimals({
             label="Sub Type"
             name="subtype"
             selectedKey={formData.subtype}
-            items={subtypes}
+            items={subtypeOptions}
             onSelectionChange={(e) => {
               handleInputChanges({ subtype: String(e) });
             }}
