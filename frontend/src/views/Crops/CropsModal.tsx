be65d2713063f559252ae168091783a081767795
Dallas Richmond <dallas.richmond@gov.bc.ca>
2025-08-26 11:25:37 -0700

    Removed console.log

--- frontend/src/views/Crops/CropsModal.tsx
+++ frontend/src/views/Crops/CropsModal.tsx
@@ -256,7 +256,6 @@ function CropsModal({
       .callEndpoint('api/wherewillpruningsgo/')
       .then((response: { status?: any; data: any }) => {
         if (response.status === 200) {
-          console.log(response.data);
           setWhereWillPruningsGo(response.data);
         }
       });
