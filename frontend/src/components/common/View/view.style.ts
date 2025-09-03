617b2423562fcc3d6068adc3c65a710a39e6e07c
Wilson Wong <wilwong89@gmail.com>
2025-09-03 02:26:24 -0700

    Change back/next buttons to position sticky at bottom of page.

--- frontend/src/components/common/View/view.style.ts
+++ frontend/src/components/common/View/view.style.ts
@@ -2,14 +2,13 @@
  * @summary Styling for AddAnimals view
  */
 import styled from '@emotion/styled';
-import { componentContainer, buttonGroup, paragraphCss } from '../../../common.styles';
+import { componentContainer, paragraphCss } from '../../../common.styles';
 
 export const StyledContent = styled.div`
   margin-bottom: 1rem;
 
   ${componentContainer}
   ${paragraphCss}
-  ${buttonGroup}
 `;
 
 export const AppTitleStyle = styled.div`
@@ -33,4 +32,12 @@ export const PageTitleStyle = styled.div`
   width: 100%;
   height: 3rem;
   padding: 1rem;
+`;
+
+export const ButtonGroupWrapper = styled.div`
+  position: sticky;
+  bottom: 0px;
+  background-color: white;
+  padding-top: 1rem;
+  padding-bottom: 1rem;
 `;
