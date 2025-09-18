3268358cf469dfb153297305b463b1e4dba756ae
Ariel Rotter-Aboyoun <raarielgrace@gmail.com>
2025-09-18 10:19:36 -0700

    PR review changes

--- frontend/src/views/Reporting/utils.ts
+++ frontend/src/views/Reporting/utils.ts
@@ -1,10 +1,14 @@
+import { jsPDF } from 'jspdf';
+
 export function getFertilizerUnitKgPerAcreConversion(unitId: number) {
   switch (unitId) {
     // Pounds per acre
     case 1:
       return 0.4536;
+    // Kilograms per hectare
     case 2:
       return 0.4047;
+    // Pounds per 1000 sq ft
     case 7:
       return 19.759;
     default:
@@ -21,10 +25,10 @@ export function getFertilizerUnitUSGallonPerAcreConversion(unitId: number) {
   switch (unitId) {
     // Litres per acre
     case 3:
-      return 3.875;
+      return 0.2642;
     // Imperial gallons per acre
     case 4:
-      return 0.8326;
+      return 1.201;
     case 5:
       return 1;
     // Litres per hectare
@@ -34,4 +38,38 @@ export function getFertilizerUnitUSGallonPerAcreConversion(unitId: number) {
       console.error(`Unrecognized liquid fertilizer unit: ${unitId}`);
       return 0;
   }
+}
+
+// https://stackoverflow.com/questions/65942761/jspdf-add-text-after-another-dynamic-text
+/**
+ * Adds text to a page at the given position and returns a position just below the text
+ * @param doc The jsPDF
+ * @param text The text to insert
+ * @param x The x-axis position of the text
+ * @param nextY The y-axis position of the text
+ * @returns The y-axis value below the added text
+ */
+export function addText(doc: jsPDF, text: string, x: number, nextY: number) {
+  doc.text(text, x, nextY, { maxWidth: 180 });
+  return Math.ceil(doc.getTextDimensions(doc.splitTextToSize(text, 180)).h) + 2 + nextY;
+}
+
+const superscriptNumberDict: { [num: string]: string } = {
+  0: '⁰',
+  1: '¹',
+  2: '²',
+  3: '³',
+  4: '⁴',
+  5: '⁵',
+  6: '⁶',
+  7: '⁷',
+  8: '⁸',
+  9: '⁹',
+};
+export function numberToSuperscript(num: number): string {
+  const str = String(num);
+  return str
+    .split('')
+    .map((c) => superscriptNumberDict[c])
+    .join();
 }
