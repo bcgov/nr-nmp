9db8ac3f04cbe7dbd3c5fcf601daa22b377c3ab3
lunamoonmoon <5862lunamoon@gmail.com>
2025-11-21 10:30:30 -0800

    implements snapshot test for FarmInformation view

--- frontend/setupFiles.js
+++ frontend/setupFiles.js
@@ -1,3 +1,11 @@
 import { TextEncoder } from 'util';
 
 global.TextEncoder = TextEncoder;
+
+jest.mock('@/env', () => ({
+  env: {
+    VITE_API_URL: 'http://localhost:5174',
+  },
+}));
+
+global.window.config = {};
---
+++ frontend/src/views/_tests_/farminformation.test.jsx
@@ -0,0 +1,37 @@
+import { render, waitFor } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import '@testing-library/jest-dom';
+import useAppState from '../../hooks/useAppState';
+import DEFAULT_NMPFILE from '../../constants/DefaultNMPFile';
+import FarmInformation from '../FarmInformation/FarmInformation';
+
+jest.mock('../../hooks/useAppState');
+const mockUseAppService = jest.mocked(useAppState);
+jest.mock('../../services/APICache', () =>
+  jest.fn().mockImplementation(() => ({
+    callEndpoint: jest.fn(() => Promise.resolve({ status: 200, data: [] })),
+  })),
+);
+
+it('renders correctly', async () => {
+  mockUseAppService.mockImplementation(() => ({
+    state: {
+      nmpFile: DEFAULT_NMPFILE,
+      showAnimalsStep: false,
+    },
+    dispatch: jest.fn(),
+  }));
+
+  const { asFragment } = render(
+    <MemoryRouter>
+      <FarmInformation />
+    </MemoryRouter>,
+  );
+
+  await waitFor(() => {
+    expect(asFragment()).toBeDefined();
+  });
+
+  // match snapshot
+  expect(asFragment()).toMatchSnapshot();
+});
