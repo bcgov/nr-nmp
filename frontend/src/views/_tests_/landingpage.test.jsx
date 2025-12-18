e10e0ebc410f2b7a8fe4cae8c7c1e67c29ebc14d
raarielgrace <raarielgrace@gmail.com>
2025-12-18 13:52:33 -0800

    test: [NR-NMP-579] Unit tests for Landing Page

--- frontend/src/views/_tests_/landingpage.test.jsx
+++ frontend/src/views/_tests_/landingpage.test.jsx
@@ -1,9 +1,10 @@
-import { render, waitFor } from '@testing-library/react';
+import { render, waitFor, screen, fireEvent } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import '@testing-library/jest-dom';
 import useAppState from '../../hooks/useAppState';
 import DEFAULT_NMPFILE from '../../constants/DefaultNMPFile';
 import LandingPage from '../LandingPage/LandingPage';
+import { FARM_INFORMATION } from '../../constants/routes';
 
 jest.mock('../../hooks/useAppState');
 const mockUseAppService = jest.mocked(useAppState);
@@ -12,6 +13,12 @@ jest.mock('../../services/APICache', () =>
     callEndpoint: jest.fn(() => Promise.resolve({ status: 200, data: [] })),
   })),
 );
+
+const mockNavigate = jest.fn();
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useNavigate: () => mockNavigate,
+}));
 
 // Snapshot test for Landing Page view
 it('renders correctly', async () => {
@@ -35,4 +42,47 @@ it('renders correctly', async () => {
 
   // match snapshot
   expect(asFragment()).toMatchSnapshot();
+});
+
+describe('Buttons perform correct functions', () => {
+  it('Get started calls hooks properly', () => {
+    const mockDispatch = jest.fn();
+    mockUseAppService.mockImplementation(() => ({
+      state: {},
+      dispatch: mockDispatch,
+    }));
+
+    render(
+      <MemoryRouter>
+        <LandingPage />
+      </MemoryRouter>,
+    );
+    const button = screen.getByText('Get Started');
+    fireEvent.click(button);
+    expect(mockDispatch).toHaveBeenCalledWith({ type: 'RESET_NMPFILE' });
+    expect(mockNavigate).toHaveBeenCalledWith(FARM_INFORMATION);
+  });
+
+  it('Upload file prompts upload', () => {
+    const mockDispatch = jest.fn();
+    mockUseAppService.mockImplementation(() => ({
+      state: {},
+      dispatch: mockDispatch,
+    }));
+    const readAsTextSpy = jest.spyOn(FileReader.prototype, 'readAsText');
+
+    const { container } = render(
+      <MemoryRouter>
+        <LandingPage />
+      </MemoryRouter>,
+    );
+    const fileInput = container.querySelector('#fileUp');
+    const str = JSON.stringify({ year: '2025' });
+    const blob = new Blob([str]);
+    const file = new File([blob], 'file.nmp', {
+      type: 'application/JSON',
+    });
+    fireEvent.change(fileInput, { target: { files: [file] } });
+    expect(readAsTextSpy).toHaveBeenCalled();
+  });
 });
